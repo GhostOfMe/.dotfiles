d5b6bfcdd3b10703d5d06eeccb64247f5bd6c141
JQ <nail.prophet@gmail.com>
2025-10-23 20:11:32 +0200

    add caffeinte to autostart

--- awesome/.config/awesome/autostart.sh
+++ awesome/.config/awesome/autostart.sh
@@ -10,7 +10,10 @@ function run {
 #run "picom -b --config  $HOME/.config/picom/picom.sample.conf --experimental-backends"
 
 run "redshift"
+run "caffeine"
+
 nitrogen --restore
+
 # nitrogen --random "/home/user/Pictures/wallpapers/nord/" --set-zoom-fill
 #setxkbmap -option grp:alt_shift_toggle us,ru
 #run 'setxkbmap -option grp:alt_shift_toggle "us,ru"'
