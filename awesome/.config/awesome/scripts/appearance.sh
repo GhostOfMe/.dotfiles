eddf1d0084a5cb084c587d946086f721e0d10dd9
JQ <nail.prophet@gmail.com>
2026-02-16 19:01:44 +0200

    fix formatting

--- awesome/.config/awesome/scripts/appearance.sh
+++ awesome/.config/awesome/scripts/appearance.sh
@@ -6,25 +6,32 @@ AWESOME_PATH="$HOME/.config/awesome/"
 category_chosen=$(printf "Color Scheme\nWibar\nTransparency\nGaps" | rofi -dmenu -i -p "Appearance")
 case "$category_chosen" in
     "Color Scheme")  
+        
         scheme_chosen=$(printf "Nord\nRosé Pine" | rofi -dmenu -i -p  "Color Scheme") 
         case "$scheme_chosen" in 
             "Nord")         $SCRIPT_PATH"/change_scheme.sh" "nord" ;;
             "Rosé Pine")    $SCRIPT_PATH"/change_scheme.sh" "rose_pine" ;;
             *) exit 1 ;;
         esac
+        
         ;;
     "Wibar") 
+        
         wibar_chosen=$(printf "Simple\nColorful" | rofi -dmenu -i -p "Wibar")
         case "$wibar_chosen" in
             "Simple") 
+                
                 rm $AWESOME_PATH"wibar_current.lua"
                 ln $AWESOME_PATH"appearance/wibar.lua" $AWESOME_PATH"wibar_current.lua"
                 echo 'awesome.restart()' | awesome-client
+                
                 ;;
             "Colorful")
+                
                 rm $AWESOME_PATH"wibar_current.lua"
                 ln $AWESOME_PATH"appearance/wibar_color.lua" $AWESOME_PATH"wibar_current.lua"
                 echo 'awesome.restart()' | awesome-client
+                
                 ;;
         esac
         ;;
