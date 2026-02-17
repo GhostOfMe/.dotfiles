ff7a99508fddd5b9d8f2bc8a820469a189c4692d
JQ <nail.prophet@gmail.com>
2026-02-17 18:55:48 +0200

    add gaps option to rofi menu

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -65,7 +65,7 @@ require("main.rules")
 
 require("main.signal")
 
-beautiful.useless_gap = 4
+beautiful.useless_gap = require("appearance.gaps")
 
 -- awful.spawn.with_shell("~/.config/awesome/random_wallpaper.sh")
 awful.spawn.with_shell("~/.config/awesome/scripts/autostart.sh")
