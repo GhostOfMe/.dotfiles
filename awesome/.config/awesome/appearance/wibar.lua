84bd6051b166a9c26874a09c6657cb4c6390aea9
JQ <nail.prophet@gmail.com>
2026-02-25 18:36:17 +0200

    add power menu button to wibar

--- awesome/.config/awesome/appearance/wibar.lua
+++ awesome/.config/awesome/appearance/wibar.lua
@@ -40,6 +40,7 @@ awful.screen.connect_for_each_screen(function(s)
         layout = wibox.layout.align.horizontal,
         { -- Left widgets
             layout = wibox.layout.fixed.horizontal,
+            appearance.my_widgets.menu(s),
             my_taglist(s),
             s.mypromptbox
         },
