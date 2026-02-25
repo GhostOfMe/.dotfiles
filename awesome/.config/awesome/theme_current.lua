0044b92b35144db7a53c2252af8c596185e1e51f
JQ <nail.prophet@gmail.com>
2026-02-25 18:39:21 +0200

    change awesome icon

--- awesome/.config/awesome/theme_current.lua
+++ awesome/.config/awesome/theme_current.lua
@@ -120,7 +120,7 @@ theme.layout_spiral = themes_path .. "icons/rp/layouts/layouts/spiral.png"
 theme.layout_dwindle = themes_path .. "icons/rp/layouts/layouts/dwindle.png"
 
 -- Generate Awesome icon:
-theme.awesome_icon = theme_assets.awesome_icon(theme.menu_height, theme.bg_focus, theme.fg_focus)
+theme.awesome_icon = theme_assets.awesome_icon(theme.menu_height, rp.n11, rp.n0)
 
 -- Define the icon theme for application icons. If not set then the icons
 -- from /usr/share/icons and /usr/share/icons/hicolor will be used.
